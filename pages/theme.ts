2f78ef6882f4b9d7cd74d388651d110455492097
Adory Vo <business@adoryvo.com>
2022-10-27 14:22:15 -0700

    Add dark theme and Turborepo

---
+++ pages/theme.ts
@@ -0,0 +1,13 @@
+// 1. Import `extendTheme` function
+import { type ThemeConfig, extendTheme } from '@chakra-ui/react'
+
+// 2. Add your color mode config
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+// 3. Extend the theme
+const theme = extendTheme({ config })
+
+export default theme
