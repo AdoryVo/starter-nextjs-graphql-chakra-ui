d5fb155e8729bbd58e4f74487bf454b4b163feee
Adory Vo <business@adoryvo.com>
2022-10-29 03:50:59 -0700

    Move theme.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 
-import theme from './theme'
+import theme from '../theme'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
